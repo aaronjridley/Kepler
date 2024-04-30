45e5bf207b9857c69aa84c68b5f2d4aa707c96e5
Aaron Ridley <ridley@umich.edu>
2024-04-30 14:18:01 -0400

    FEAT: drivers for MAAX orbits

---
+++ Earth/maax.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+DATE=20100216
+ALT=37000
+
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxA_ltan00_inc90 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxA_ltan03_inc90 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxA_ltan06_inc90 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxA_ltan09_inc90 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxA_ltan00_inc80 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxA_ltan03_inc80 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxA_ltan06_inc80 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxA_ltan09_inc80 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxA_ltan00_inc70 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxA_ltan03_inc70 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxA_ltan06_inc70 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxA_ltan09_inc70 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan00_inc90_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan03_inc90_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan06_inc90_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan09_inc90_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan00_inc80_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan03_inc80_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan06_inc80_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan09_inc80_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan00_inc70_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan03_inc70_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan06_inc70_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan09_inc70_delay050 -delay=-0.5 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan00_inc90_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan03_inc90_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan06_inc90_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+#./kepler.py -starttime=${DATE}:00 -inc=90 -time=336 -sat=maaxB_ltan09_inc90_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan00_inc80_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan03_inc80_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan06_inc80_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+./kepler.py -starttime=${DATE}:00 -inc=80 -time=336 -sat=maaxB_ltan09_inc80_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9
+
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan00_inc70_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=0
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan03_inc70_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=3
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan06_inc70_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=6
+#./kepler.py -starttime=${DATE}:00 -inc=70 -time=336 -sat=maaxB_ltan09_inc70_delay025 -delay=-0.25 -perigee=${ALT} -apogee=${ALT} -dt=15 -ltan=9

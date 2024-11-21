8570df5be8de74eadd9d3a4047dbf21b0fe50ce2
Aaron Ridley <ridley@umich.edu>
2024-11-21 13:40:16 -0500

    FEAT: Dynamically alter mass flow rate to get to orbit

--- OrbitalInsertion/launch.py
+++ OrbitalInsertion/launch.py
@@ -27,11 +27,11 @@ vRotation = 465.1  # m/s <- rotational speed of Earth at equator
 
 orbitAltitude = 400.0 * 1000.0  # km -> converted to meters
 
-dt = 0.1
-radius = 0.25 # m
+dt = 0.01
+radius = 0.3 # m
 Cd = 0.29 # drag coefficient
 mass0 = 250 # kg this is the wet pass
-payloadMass = 5 # kg
+payloadMass = 25 # kg
 rocketMass = mass0 * 0.1 # assume rocket body + engine = 10% of wet mass
 alt0 = 6000 # meters
 v0Vertical = 8.0 * soundSpeed  # assume initial launch is vertical
@@ -42,7 +42,8 @@ Ve = Isp * 9.8 # exhaust velocity in m/s
 area = pi * radius**2
 ballCoef = Cd * area
 
-massFlowRate = 10.0 # kg/s <---- this is the critical setting!
+massFlowRate = 5.0 # kg/s <---- this is a critical setting!
+maxAcc = 5.0 * 9.8 # in m/s2 <--- basically don't want parts to break
 
 def calc_altitude_reached(currentAltitude, currentSpeed):
     r1 = R0 + currentAltitude
@@ -84,6 +85,9 @@ print('DeltaV needed and DeltaV from Rocket Equation (m/s) : ', \
 
 drag = calc_drag(alt0, v0Horizontal, v0Vertical, ballCoef)
 
+massFlowRate = np.abs(drag[1]) / Ve * 1.001
+
+print('mfr : ', massFlowRate)
 totalThrust = Ve * massFlowRate
 # Want to thrust in the vertical direction just enough to counteract drag
 vThrust = -drag[1]
@@ -97,11 +101,123 @@ print('Initial Accelerations (hor, ver, in m/s) : ', hAcc0, vAcc0)
 
 times = [0.0]
 alts = [alt0]
-vVel = [v0Vertical]
-hVel = [v0Horizontal]
-vAcc = []
-hAcc = []
-mass = [mass0]
+hPoss = [0.0]
+vVels = [v0Vertical]
+hVels = [v0Horizontal]
+vAccs = [vAcc0]
+hAccs = [hAcc0]
+masses = [mass0]
+thrustAcc = [totalThrust / mass0]
+
+hVelGoal = calc_orbital_speed(orbitAltitude)
+
+i = 0
+while ((masses[-1] > rocketMass + payloadMass) & \
+       (alts[-1] < orbitAltitude)):
+    
+    # Positions:
+    alt = alts[-1]
+    hPos = hPoss[-1]
+    # Velocities:
+    vV = vVels[-1]
+    hV = hVels[-1]
+    # current mass:
+    mass = masses[-1]
+
+    # Drag:
+    drag = calc_drag(alt, hV, vV, ballCoef)
+
+    if (hV < hVelGoal):
+        accMag = np.sqrt(hAccs[-1]**2 + vAccs[-1]**2)
+        if ((accMag > maxAcc) & (Ve * massFlowRate * 0.98 > np.abs(drag[1]))):
+            # slowly reduce mass flow rate if accelerating too fast
+            massFlowRate = massFlowRate * 0.99
+            print('Limiting Mfr : ', massFlowRate)
+        if (Ve * massFlowRate < np.abs(drag[1])):
+            massFlowRate = np.abs(drag[1]) / Ve * 1.1
+    else:
+        massFlowRate = 0.0
+ 
+    # thrust:
+    totalThrust = Ve * massFlowRate
+
+    if (totalThrust > 0):
+        # Want to thrust in the vertical direction just enough to counteract drag
+        vThrust = -drag[1]
+        # put the rest of the thrust in the horizontal direction
+        hThrust = np.sqrt(totalThrust**2 - vThrust**2)
+    else:
+        vThrust = 0.0
+        hThrust = 0.0
+        
+    # calculate accelerations:
+    hAcc = drag[0]/mass + hThrust/mass
+    vAcc = drag[1]/mass + gravity(alt) + vThrust/mass
+
+    thrustAcc.append(totalThrust / mass)
+    vAccs.append(vAcc)
+    hAccs.append(hAcc)
+    
+    accMag = np.sqrt(hAcc**2 + vAcc**2) / 9.8
+    
+    # update velocities:
+    vVels.append(vV + vAcc * dt)
+    hVels.append(hV + hAcc * dt)
+
+    # update positions:
+
+    alt = alt + vV * dt + 0.5 * vAcc * dt * dt
+    hPos = hPos + hV * dt + 0.5 * hAcc * dt * dt
+
+    alts.append(alt)
+    hPoss.append(hPos)
+
+    mass = mass - massFlowRate * dt
+    masses.append(mass)
+
+    times.append(times[-1] + dt)
+    
+    percentSpeed = int(hV / hVelGoal * 100.0)
+    print('time, alt, hVel :',
+          int(times[-1]),
+          int(alt/1000.0),
+          int(hV), percentSpeed, int(mass), accMag)
+
+    i += 1
+
+fig = plt.figure(figsize = (10,10))
+ax = fig.add_subplot(411)
+ax2 = fig.add_subplot(412)
+ax3 = fig.add_subplot(413)
+ax4 = fig.add_subplot(414)
+
+ax.axhline(orbitAltitude/1000.0, label = 'Orbital Alt.')
+ax.plot(times, np.array(alts)/1000.0, label = 'Altitude')
+ax.plot(times, np.array(hPoss)/1000.0, label = 'Down Range')
+ax.set_ylabel('Distance (km)')
+ax.legend()
+
+ax2.plot(times, np.array(vVels), label = 'Vertical Velocity')
+ax2.plot(times, np.array(hVels), label = 'Horizontal Velocity')
+ax2.set_ylabel('Velocity (m/s)')
+ax2.legend()
+
+ax3.plot(times, np.array(vAccs)/9.8, label = 'Vertical Accel.')
+ax3.plot(times, np.array(hAccs)/9.8, label = 'Horizontal Accel.')
+ax3.plot(times, np.array(thrustAcc)/9.8, label = 'Thrust Acc')
+ax3.set_ylabel('Acceleration (g)')
+ax3.legend()
+
+ax4.plot(times, np.array(masses), label = 'Total Mass (kg)')
+ax4.axhline(payloadMass + rocketMass, label = 'Dry Mass', color = 'k')
+ax4.set_ylim(0.0, mass0)
+ax4.set_ylabel('Mass (kg)')
+ax4.set_xlabel('Time (s)')
+ax4.legend()
 
 
+plotfile = 'alt.png'
+print('writing : ',plotfile)    
+fig.savefig(plotfile)
+plt.close()
 
